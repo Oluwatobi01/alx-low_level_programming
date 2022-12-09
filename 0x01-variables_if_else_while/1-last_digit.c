103ba7b7c13034abf051fa972f33a4d72866d625
Oluwatobi01 <isaacabraham028@gmail.com>
2022-12-09 03:30:23 -0800

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
  *
  */
 
-int main()
+int main(void)
 {
 
 	int n;
@@ -21,17 +21,14 @@ int main()
 	num = n % 10;
 	if (num > 5)
 	{
-	
 		printf("%d and is greater than 5\n", num);
 	}
 	else if ((num < 6) && (num < 0))
 	{
-	
 		printf("%d and is less than 6 and not 0\n", num);
 	}
 	else
 	{
-	
 		printf("%d and is 0\n", num);
 	}
 	return (0);
