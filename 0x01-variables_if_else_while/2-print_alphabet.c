4928bd2cce2d9d17ccec5302c4ea9bbdc2217575
Oluwatobi01 <isaacabraham028@gmail.com>
2022-12-09 02:46:00 -0800

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabeth
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+	
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
