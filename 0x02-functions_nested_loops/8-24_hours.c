d520b548e3f8c1cc364765dbf9de9d41cb657b9a
Oluwatobi01 <isaacabraham028@gmail.com>
2022-12-13 05:31:11 -0800

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ void jack_bauer(void)
 	int a;
 	int b;
 
-	for (int hour = 0; hour < 24; hour++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (int minute = 0; minute < 60; minute++)
+		for (b = 0; b <= 59; b++)
 		{
 			_putchar (a / 10 + '0');
 			_putchar (a % 10 + '0');
