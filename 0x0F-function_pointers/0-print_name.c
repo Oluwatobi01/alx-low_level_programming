143bd5f835a92af866c92ca7b85c3fc0895283e0
Oluwatobi01 <isaacabraham028@gmail.com>
2023-01-18 15:56:11 -0800

    tasj0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * @name: char to display to the stdout
  * @f: A pointer function
  * Return: No return cause we've declared VOID as R_TYPE
-*/
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
