c16812feea44dddc7403a7b318dc667d0003674e
Oluwatobi01 <isaacabraham028@gmail.com>
2023-01-18 16:44:27 -0800

    op

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+
 /**
  * op_add - adds @a and @b
  * 
@@ -11,6 +12,7 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
  * op_sub - gets the difference of @a and @b
  *
@@ -23,6 +25,7 @@ int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
  * op_mul - multiplies @a and @b
  *
@@ -35,6 +38,7 @@ int op_div(int a, int b)
 {
 	return (a / b);
 }
+
 /**
  * op_mod - gets the reminder of the division between @a and @b
  *
