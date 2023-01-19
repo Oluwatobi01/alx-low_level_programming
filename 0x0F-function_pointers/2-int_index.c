9298a50bc5de9196e296d9678b69924d102ba0f7
Oluwatobi01 <isaacabraham028@gmail.com>
2023-01-18 16:07:35 -0800

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,8 +4,8 @@
  * int_index - earches for an integer
  * @array: array to search in
  * @size: size of the array
- * 
  * @cmp: pointer to the comparing function
+ *
  * Return: index of the first element for which
  * the cmp function does not return 0, or -1 if no match is found
  * or size is negative
