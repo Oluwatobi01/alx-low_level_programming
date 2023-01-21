d3bbc6406de501176515183b74c14c7e5f217a35
Oluwatobi01 <isaacabraham028@gmail.com>
2023-01-21 08:03:10 -0800

    task that delayed me

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,3 @@
-#include "3-calc.h"
-
 /**
  * op_add - adds @a and @b
  *
@@ -7,11 +5,13 @@
  * @b: input 2
  *
  * Return: sum of @a and @b
- */
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 
 /**
  * op_sub - gets the difference of @a and @b
@@ -20,11 +20,12 @@ int op_add(int a, int b)
  * @b: input 2
  *
  * Return: difference of @a and @b
- */
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 
 /**
  * op_mul - multiplies @a and @b
@@ -32,12 +33,27 @@ int op_sub(int a, int b)
  * @a: input 1
  * @b: input 2
  *
+ * Return: product of @a and @b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides @a by @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
  * Return: results of the division @a and @b
- */
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
 
 /**
  * op_mod - gets the reminder of the division between @a and @b
@@ -46,7 +62,7 @@ int op_div(int a, int b)
  * @b: input 2
  *
  * Return: the remainder
- */
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
